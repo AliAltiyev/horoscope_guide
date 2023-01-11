08791ac21b8fd0278cf6f2ca5f07aa76dbce9fcc
AliAltiyev <alidroid696@gmail.com>
2023-01-11 22:57:14 +0300

    Using TextFormField with validators and using Form with wrapping children

--- lib/widgest/text_form.dart
+++ lib/widgest/text_form.dart
@@ -8,6 +8,9 @@ class TextFormExample extends StatefulWidget {
 }
 
 class _TextFormExampleState extends State<TextFormExample> {
+  String? email, password;
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,69 +20,92 @@ class _TextFormExampleState extends State<TextFormExample> {
           child: Column(
             children: <Widget>[
               Form(
+                key: formKey,
                 autovalidateMode: AutovalidateMode.onUserInteraction,
-                child: TextFormField(
-                  validator: (value) {
-                    if (value!.length < 10) {
-                      return "Your password is too short";
-                    } else {
-                      return null;
-                    }
-                  },
-                  decoration: const InputDecoration(
-                      label: Text("Email"), prefixIcon: Icon(Icons.email)),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        onSaved: (value) {
+                          email = value;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Enter email";
+                          } else if (value!.length < 10) {
+                            return "Your email is too short";
+                          } else {
+                            return null;
+                          }
+                        },
+                        decoration: const InputDecoration(
+                            label: Text("Email"),
+                            prefixIcon: Icon(Icons.email)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        onSaved: (value) {
+                          password = value;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Enter password";
+                          } else if (value!.length < 5) {
+                            return "Your password is too short";
+                          } else {
+                            return null;
+                          }
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.email),
+                          labelText: "Password",
+                          fillColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.blue,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.blue,
+                              width: 1.0,
+                            ),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.red,
+                              width: 1.0,
+                            ),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: const BorderSide(
+                              color: Colors.red,
+                              width: 1.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          final result = formKey.currentState?.validate();
+                          if (result == true) {
+                            formKey.currentState?.save();
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text("$email $password")));
+                          }
+                        },
+                        child: const Text("Log in"))
+                  ],
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Form(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  child: TextFormField(
-                    validator: (value) {
-
-                      if (value!.length < 5) {
-                        return "Your password is too short";
-                      } else {
-                        return null;
-                      }
-                    },
-                    decoration: InputDecoration(
-                        labelText: "Enter Email",
-                        fillColor: Colors.white,
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                          borderSide: const BorderSide(
-                            color: Colors.blue,
-                          ),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                          borderSide: const BorderSide(
-                            color: Colors.blue,
-                            width: 1.0,
-                          ),
-                        ),
-                        errorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                          borderSide: BorderSide(
-                            color: Colors.red,
-                            width: 1.0,
-                          ),
-                        ),
-                        focusedErrorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                          borderSide: const BorderSide(
-                            color: Colors.red,
-                            width: 1.0,
-                          ),
-                        ),
-                        prefixIcon: const Icon(Icons.email)),
-                  ),
-                ),
-              ),
+              )
             ],
           ),
         ),
