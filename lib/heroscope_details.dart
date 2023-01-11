3b60cd447c32b5b25373c96a4242094d06c6c454
AliAltiyev <alidroid696@gmail.com>
2023-01-11 14:34:51 +0300

    Add WidgetsBinding.instance .addPostFrameCallback((timeStamp) => to work PaletteGenerator in initState

--- lib/heroscope_details.dart
+++ lib/heroscope_details.dart
@@ -1,28 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:horoscope_guide/model/heroscope_model.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-class HeroScopeDetails extends StatelessWidget {
+class HeroScopeDetails extends StatefulWidget {
   final HeroScopeModel heroScopeModel;
-  const HeroScopeDetails({Key? key, required this.heroScopeModel}) : super(key: key);
+
+  const HeroScopeDetails({Key? key, required this.heroScopeModel})
+      : super(key: key);
+
+  @override
+  State<HeroScopeDetails> createState() => _HeroScopeDetailsState();
+}
+
+class _HeroScopeDetailsState extends State<HeroScopeDetails> {
+  Color appbarColor = Colors.purple;
+  late PaletteGenerator _paletteGenerator;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((timeStamp) => _generateAppBarColor());
+  }
 
   @override
   Widget build(BuildContext context) {
-    debugPrint(heroScopeModel.toString());
-    return  Scaffold(
+    debugPrint(widget.heroScopeModel.toString());
+    return Scaffold(
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            elevation: 10,
+            backgroundColor: appbarColor,
             expandedHeight: 250,
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
               background: Image.asset(
-                "assets/images/${heroScopeModel.largeImage}",
+                "assets/images/${widget.heroScopeModel.largeImage}",
                 fit: BoxFit.cover,
               ),
               centerTitle: true,
-              expandedTitleScale: 2,
-              title: Text("Heroscope ${heroScopeModel.name}"),
+              expandedTitleScale: 1.5,
+              title: Text("Heroscope ${widget.heroScopeModel.name}"),
             ),
           ),
           SliverToBoxAdapter(
@@ -31,10 +49,15 @@ class HeroScopeDetails extends StatelessWidget {
               child: Column(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(bottom: 40,top: 20),
-                    child: Text(heroScopeModel.time,style: const TextStyle(fontSize: 24,fontWeight: FontWeight.w600)),
+                    padding: const EdgeInsets.only(bottom: 40, top: 20),
+                    child: Text(widget.heroScopeModel.time,
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w600)),
                   ),
-                  Text(heroScopeModel.description,style: const TextStyle(fontSize: 18),),
+                  Text(
+                    widget.heroScopeModel.description,
+                    style: const TextStyle(fontSize: 18),
+                  ),
                 ],
               ),
             ),
@@ -42,5 +65,14 @@ class HeroScopeDetails extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void _generateAppBarColor() async {
+    _paletteGenerator = await PaletteGenerator.fromImageProvider(
+        AssetImage("assets/images/${widget.heroScopeModel.largeImage}"));
+    setState(() {});
+    appbarColor = _paletteGenerator.dominantColor!.color;
+    debugPrint(
+        " afkjniucnewiucvewiuvbewybvuewybvuyewbvu${appbarColor.toString()}");
   }
 }
